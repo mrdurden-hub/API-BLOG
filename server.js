e69fe096a3e5536e172b879d55aceec9a72b7e33
mrdurden-hub <cmateus297@gmail.com>
2021-10-04 20:48:05 -0300

    heroku config

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ app.use(routes);
 app.use(usersRoutes);
 
 app.on('connected', () => {
-  app.listen(process.env.PORT, () => {
-    console.log(`Server running at http://localhost:${process.env.PORT}`);
+  app.listen(process.env.PORT || 3000, () => {
+    console.log('Server running');
   });
 });
